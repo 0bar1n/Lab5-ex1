de9972c06391ca7b1d0b5beb853cc80f3814cc92
barinskaya <1388zxc@gmail.com>
2022-09-30 15:23:19 +0300

    Добавьте файлы проекта.

---
+++ Практическая5 пример 1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace HelloApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Tom"; int age = 33; //задание переменной строкового типа ; целочисленного типа
+            bool isEmployed = false; double weight = 78.65; //задание переменной логического типа; вещественного типа
+            Console.WriteLine($"Имя: {name}"); Console.WriteLine($"Возраст: {age}");   //вывод переменной; вывод переменной
+            Console.WriteLine($"Вес: {weight}"); Console.WriteLine($"Работает: {isEmployed}"); //вывод переменной; вывод переменной
+        }
+    }
+}
